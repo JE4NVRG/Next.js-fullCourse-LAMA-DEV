89a000b0a4c6698d3458aa1c60363dcbfbfce20b
JE4NVRG <jean.v1803@gmail.com>
2023-09-20 14:24:23 -0300

    Tudo ok ate aqui

---
+++ src/app/portfolio/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PortfolioPage = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>This app uses React, Redux, React Router, and many other helpful libraries.</p>
+    </div>
+  );
+}
+
+export default PortfolioPage;
