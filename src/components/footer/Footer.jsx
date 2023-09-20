07417b7343f742ebfb99b7274215327ec33ca85f
JE4NVRG <jean.v1803@gmail.com>
2023-09-20 19:15:52 -0300

    adicionado icons

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import styles from './footer.module.css'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
     <div className={styles.container}>
-        <diuv>©2023 Je4nDev. All rights reserved.</diuv>
-        <div></div>
+        <div>©2023 Je4nDev. All rights reserved.</div>
+        <div className={styles.social}>
+          <Image src="/1.png" width={20} height={20} className={styles.icon} />
+          <Image src="/2.png" width={20} height={20} className={styles.icon}/>
+          <Image src="/3.png" width={20} height={20} className={styles.icon}/>
+          <Image src="/4.png" width={20} height={20} className={styles.icon}/>
+        </div>
     </div>
   )
 }
