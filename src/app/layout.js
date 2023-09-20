8a9bd05c7437652b5a7ea73bc4937e045515e764
JE4NVRG <jean.v1803@gmail.com>
2023-09-20 16:34:23 -0300

    atualizado

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
+import Footer from '@/components/navbar/footer/Footer'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter, Roboto, Poppins } from 'next/font/google'
+import NavBar from '@/components/navbar/footer/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,8 +12,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        <NavBar/>
+        {children}</body>
+        <Footer/>
     </html>
-  )
+    );
 }
---
+++ src/components/navbar/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <diuv>©2023 Je4nDev. All rights reserved.</diuv>
+        <div></div>
+    </div>
+  )
+}
+
+export default Footer
