a16ceb4f888b9d4798ffcfb116a79848f638c8c2
JE4NVRG <jean.v1803@gmail.com>
2023-09-21 00:16:55 -0300

    melhorada na home, porem falta coisa ainda

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,8 +4,21 @@ import Hero from 'public/hero.png'
 
 export default function Home() {
   return (
-    <div>
-      <Image src={Hero} alt="" />
+    <div className={styles.container}>
+    <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products
+        </h1>
+        <p className={styles.desc}>
+          Turning yout Idea into Reality. We bring together the teams from the
+          global tech industry
+        </p>  
+        <button className={styles.button}>See Our Works</button>         
+      </div>
+      <div className={styles.item}></div>
+      <Image src={Hero} alt="" className={styles.img} />
     </div>
-  )
+    
+    
+  );
 }
